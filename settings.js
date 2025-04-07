203192dfefa4b6350796ccbb6d996d94fc2ea8a1
johnrickman709 <osarumwenseosadolor85@gmail.com>
2025-04-07 14:09:16 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,12 +7,12 @@
 const fs = require('fs')
 const chalk = require('chalk')
 //contact details
-global.ownernomer = "255756530143"
-global.ownername = "HANSTZ"
+global.ownernomer = "2349153516126"
+global.ownername = "CAESARZ"
 global.ytname = "HANSTZ-TECH"
 global.socialm = "GitHub: Mrhanstz"
 global.location = "AFRICA, TANZANIA, DODOMA"
-global.ownernumber = '255756530143'  //creator number
+global.ownernumber = '2349153516126'  //creator number
 global.ownername = 'HANSTZ' //owner name
 global.botname = 'HANS-XMD' //name of the bot
 //sticker details
